21d556e2ad26b5daed1847c13f4c46d7c9cb2421
httung12303 <blackzerob562@gmail.com>
2022-09-11 23:17:31 +0700

    Add change color on mouseenter function

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 addGridDivs(16);
 
+const gridItems = document.querySelectorAll('.grid-item');
+gridItems.forEach(gridItem => gridItem.addEventListener('mouseenter', changeColor));
+
+
+function changeColor() {
+    this.style.backgroundColor = 'pink';
+    console.log(this);
+}
+
 function addGridDivs(size) {
     const grid = document.querySelector('.grid');
     for(let i = 0; i < size * size; i++) {
